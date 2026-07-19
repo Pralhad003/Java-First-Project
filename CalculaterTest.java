7c8357af820ee2c4a64a214328bdbbb4f11b2f1c
Pralhad Bhusari <bhusaripralhad11@gmail.com>
2026-07-19 13:10:55 +0530

    new file added addtion

---
+++ CalculaterTest.java
@@ -0,0 +1,11 @@
+class Calculater{
+public int add(int a,int b){
+    return a+b;
+}
+}
+public class CalculaterTest {
+    public static void main(String[] args) {
+        Calculater c=new Calculater();
+        System.out.println("result:"+c.add(10,20));
+    }
+}
