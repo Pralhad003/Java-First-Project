97e783450fa28483d687455a061a0823504adf70
Pralhad Bhusari <bhusaripralhad11@gmail.com>
2026-07-19 14:04:53 +0530

    added Substraction features

--- CalculaterTest.java
+++ CalculaterTest.java
@@ -5,11 +5,15 @@ public int add(int a,int b){
 public int mult(int a,int b){
     return a*b;
 }
+public int sub(int a,int b){
+    return a-b;
+}
 }
 public class CalculaterTest {
     public static void main(String[] args) {
         Calculater c=new Calculater();
         System.out.println("result:"+c.add(10,20));
         System.out.println("result"+c.mult(10,20));
+        System.out.println("result"+c.sub(10,20));
     }
 }
