160c6b2e21bad343c54f3ff979ba7eeddcba18aa
Pralhad Bhusari <bhusaripralhad11@gmail.com>
2026-07-19 14:02:09 +0530

    added multiplication features

--- CalculaterTest.java
+++ CalculaterTest.java
@@ -2,10 +2,14 @@ class Calculater{
 public int add(int a,int b){
     return a+b;
 }
+public int mult(int a,int b){
+    return a*b;
+}
 }
 public class CalculaterTest {
     public static void main(String[] args) {
         Calculater c=new Calculater();
         System.out.println("result:"+c.add(10,20));
+        System.out.println("result"+c.mult(10,20));
     }
 }
